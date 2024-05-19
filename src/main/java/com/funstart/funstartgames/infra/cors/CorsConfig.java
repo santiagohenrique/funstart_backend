afc363da22496e16f51a148802e4a68353eb42f0
Henrique Santiago <henrique11santiagopires@hotmail.com>
2024-05-19 11:15:43 -0300

    CORS update

--- src/main/java/com/funstart/funstartgames/infra/cors/CorsConfig.java
+++ src/main/java/com/funstart/funstartgames/infra/cors/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173/funstart_frontend")
+                .allowedOrigins("http://localhost:5173")
                 .allowedMethods("GET", "POST");
     }
 }
